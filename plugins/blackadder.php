d8549147e881a33b467a96f95e6e64eb3bbe4767
pdewouters <paul@hmn.md>
2013-05-27 10:06:16 -0300

    changed command to sayquote and accept source argument

--- plugins/blackadder.php
+++ plugins/blackadder.php
@@ -1,13 +1,11 @@
 <?php
-/*
-Plugin Name: Random Blackadder quotes
-*/
-
-/*
- * Posts a random blackadder quote
+/**
+ * Plugin name: Random Quotes
+ * Displays a random quote
+ * Class BlackAdder
  */
 
-class BlackAdder {
+class Random_Quote {
 
 	public $quotes;
 
@@ -15,36 +13,48 @@ class BlackAdder {
 	function __construct() {
 
 		// Watch channel messages
-		add_action( 'on_channel', array(&$this, 'command_searcher'), 10, 2 );
+		add_action( 'on_channel', array( &$this, 'command_searcher' ), 10, 2 );
 
 		// load quotes
-		$quotes = file_get_contents('quotes.json');
+		$quotes = file_get_contents( 'quotes.json' );
 
-		$this->quotes = json_decode($quotes);
+		$this->quotes = json_decode( $quotes );
+
 	}
 
-	// Parses channel messages looking for !blackadder
+	// Parses channel messages looking for !sayquote
 	public function command_searcher( $irc, $data ) {
 
 		// Parse the message and extract the result into variables
 		$c = parse_command( $data->message );
-		if ( !$c )
+		if ( ! $c )
 			return;
+
 		extract( $c );
 
-		// We're only interested in the "blackadder" command
-		if ( 'blackadder' != $c )
+		// Abort if the command is unknown to this plugin
+		if ( ! in_array( $command, array( 'sq', 'sayquote' ) ) )
 			return;
 
-		// get a random quote ID
-		$quote_id = array_rand($this->quotes->blackadder,1);
-		
-		$quote = $this->quotes->blackadder[$quote_id]->quote_text;
+		// Make sure there was a search query
+		if ( empty( $query ) )
+			return $irc->message( SMARTIRC_TYPE_NOTICE, $msgdata->nick, 'You must enter a source for the quote (e.g.
+			blackadder.');
 
+		// set subset of quotes to value of argument
+		$source = $this->quotes->$query;
+
+		//get a random quote ID
+		$quote_id = array_rand( $source, 1 );
+
+		// fetch the quote
+		$quote = $source[$quote_id]->quote_text;
 
 		// Send the line to the channel that the command was said in
 		$irc->message( SMARTIRC_TYPE_QUERY, $data->channel, $quote );
+
 	}
+
 }
 
-$blackadder = new BlackAdder();
+$random_quote = new Random_Quote();
